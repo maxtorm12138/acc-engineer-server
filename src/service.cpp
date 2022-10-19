bb9d7dc87197c6a0e0ef80d224e99104f08fba57
maxtorm <maxtorm12138@gmail.com>
2022-10-19 18:59:03 +0800

    rpc timeout

--- src/service.cpp
+++ src/service.cpp
@@ -19,10 +19,9 @@ namespace acc_engineer
     {
         using namespace std::placeholders;
         running_ = true;
-        method_group_.implement<Echo>(0, std::bind(&service::echo, this, _1));
+        method_group_.implement<Echo>(std::bind(&service::echo, this, _1));
 
-        net::co_spawn(co_await net::this_coro::executor, unix_domain_run(), net::detached);
-        net::co_spawn(co_await net::this_coro::executor, udp_read_run(), net::detached);
+        net::co_spawn(co_await net::this_coro::executor, udp_run(), net::detached);
         co_await net::co_spawn(co_await net::this_coro::executor, tcp_run(), net::use_awaitable);
     }
 
@@ -48,93 +47,34 @@ namespace acc_engineer
         }
     }
 
-    net::awaitable<void> service::udp_read_run()
+    net::awaitable<void> service::udp_run()
     {
         net::ip::udp::socket acceptor(co_await net::this_coro::executor, net::ip::udp::endpoint{config_.address(), config_.port()});
         acceptor.set_option(boost::asio::socket_base::reuse_address(true));
-        std::vector<uint8_t> buffer(8192);
+
+        std::string initial(1500, '\0');
         while (running_)
         {
             net::ip::udp::endpoint remote;
-            size_t size_read = co_await acceptor.async_receive_from(net::buffer(buffer), remote, net::use_awaitable);
+            size_t size_read = co_await acceptor.async_receive_from(net::buffer(initial), remote, net::use_awaitable);
 
-            if (!ep_unix_socket_.contains(remote))
+            net::ip::udp::socket socket(co_await net::this_coro::executor);
+            if (remote.address().is_v4())
             {
-                net::local::stream_protocol::endpoint endpoint("acc-engineer-server.socket");
-                auto unix_socket = std::make_shared<unix_socket_t>(co_await net::this_coro::executor);
-                unix_socket->connect(endpoint);
-
-                ep_unix_socket_.emplace(remote, unix_socket);
-                net::co_spawn(co_await net::this_coro::executor, udp_write_run(acceptor, remote, unix_socket), net::detached);
+                socket.open(net::ip::udp::v4());
             }
+            else if (remote.address().is_v6())
+            {
+                socket.open(net::ip::udp::v6());
+            }
+            socket.set_option(net::socket_base::reuse_address(true));
+            socket.bind({config_.address(), config_.port()});
+            socket.connect(remote);
 
-            co_await net::async_write(*ep_unix_socket_[remote], net::buffer(buffer, size_read), net::use_awaitable);
+            auto udp_stub = std::make_shared<udp_stub_t>(std::move(socket), method_group_);
+            id_udp_stub_.emplace(udp_stub->id(), udp_stub);
+            co_await udp_stub->run(net::buffer(initial));
         }
-    }
-
-    net::awaitable<void> service::udp_write_run(
-            net::ip::udp::socket &acceptor,
-            net::ip::udp::endpoint remote, std::shared_ptr<unix_socket_t> unix_socket)
-    {
-        while (running_)
-        {
-            uint64_t command_id = 0;
-            uint64_t flags = 0;
-            uint64_t cookie_payload_size = 0;
-            uint64_t message_payload_size = 0;
-
-            std::array header_payload
-                    {
-                            net::buffer(&command_id, sizeof(command_id)),
-                            net::buffer(&flags, sizeof(flags)),
-                            net::buffer(&cookie_payload_size, sizeof(cookie_payload_size)),
-                            net::buffer(&message_payload_size, sizeof(message_payload_size)),
-                    };
-
-
-            co_await net::async_read(*unix_socket, header_payload, net::use_awaitable);
-            rpc::payload_t cookie_payload(cookie_payload_size, '\0');
-            rpc::payload_t message_payload(message_payload_size, '\0');
-
-            std::array variable_data_payload
-                    {
-                            net::buffer(cookie_payload),
-                            net::buffer(message_payload)
-                    };
-            co_await net::async_read(*unix_socket, variable_data_payload, net::use_awaitable);
-
-            const std::array payloads
-                    {
-                            net::buffer(&command_id, sizeof(command_id)),
-                            net::buffer(&flags, sizeof(flags)),
-                            net::buffer(&cookie_payload_size, sizeof(cookie_payload_size)),
-                            net::buffer(&message_payload_size, sizeof(message_payload_size)),
-                            net::buffer(cookie_payload),
-                            net::buffer(message_payload)
-                    };
-
-            co_await acceptor.async_send_to(payloads, remote, net::use_awaitable);
-        }
-    };
-
-    net::awaitable<void> service::unix_domain_run()
-    {
-        net::local::stream_protocol::endpoint endpoint("acc-engineer-server.socket");
-        net::local::stream_protocol::acceptor acceptor(co_await net::this_coro::executor, endpoint);
-
-        while (running_)
-        {
-            unix_socket_t unix_socket(co_await net::this_coro::executor);
-            co_await acceptor.async_accept(unix_socket, net::use_awaitable);
-            net::co_spawn(co_await net::this_coro::executor, new_unix_domain_connection(std::move(unix_socket)), net::detached);
-        }
-    }
-
-    net::awaitable<void> service::new_unix_domain_connection(unix_socket_t socket)
-    {
-        auto unix_domain_stub = std::make_shared<unix_domain_stub_t>(std::move(socket), method_group_);
-        id_unix_domain_stub_.emplace(unix_domain_stub->id(), unix_domain_stub);
-        co_await unix_domain_stub->run();
     }
 
     net::awaitable<Echo::Response> service::echo(const Echo::Request &request)
--- src/service.h
+++ src/service.h
@@ -6,8 +6,6 @@
 #include <boost/asio/ip/tcp.hpp>
 #include <boost/asio/ip/udp.hpp>
 #include <boost/asio/awaitable.hpp>
-#include <boost/asio/local/stream_protocol.hpp>
-
 
 #include "rpc/stub.h"
 #include "config.h"
@@ -18,9 +16,8 @@ namespace acc_engineer
 {
     namespace net = boost::asio;
 
-    using tcp_stub_t = rpc::stub<net::ip::tcp::socket>;
-    using unix_socket_t = net::local::stream_protocol::socket;
-    using unix_domain_stub_t = rpc::stub<unix_socket_t>;
+    using tcp_stub_t = rpc::stream_stub<net::ip::tcp::socket>;
+    using udp_stub_t = rpc::datagram_stub<net::ip::udp::socket>;
 
     class service
     {
@@ -32,29 +29,22 @@ namespace acc_engineer
     private:
         net::awaitable<void> tcp_run();
 
-        net::awaitable<void> udp_read_run();
-
-        net::awaitable<void> udp_write_run(
-                net::ip::udp::socket &acceptor,
-                net::ip::udp::endpoint remote, std::shared_ptr<unix_socket_t> unix_socket);
-
-        net::awaitable<void> unix_domain_run();
-
+        net::awaitable<void> udp_run();
 
         net::awaitable<void> new_tcp_connection(net::ip::tcp::socket socket);
 
-        net::awaitable<void> new_unix_domain_connection(unix_socket_t socket);
+        net::awaitable<void> new_udp_connection(net::ip::udp::socket socket);
 
         net::awaitable<Echo::Response> echo(const Echo::Request &request);
         //net::awaitable<rpc::result<Authentication::Response>> authentication(const Authentication::Request &request);
+
 
         config config_;
         bool running_{false};
         rpc::method_group method_group_;
 
         std::unordered_map<uint64_t, std::shared_ptr<tcp_stub_t>> id_tcp_stub_;
-        std::unordered_map<uint64_t, std::shared_ptr<unix_domain_stub_t>> id_unix_domain_stub_;
-        std::unordered_map<net::ip::udp::endpoint, std::shared_ptr<unix_socket_t>> ep_unix_socket_;
+        std::unordered_map<uint64_t, std::shared_ptr<udp_stub_t>> id_udp_stub_;
     };
 }
 
