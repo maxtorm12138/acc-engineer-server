c1b6022cba918d22c2b1c7daf542946c8fe7d729
maxtorm <maxtorm12138@gmail.com>
2022-10-26 11:39:18 +0800

    batch task

---
+++ src/rpc/batch_task.h
@@ -0,0 +1,10 @@
+#ifndef ACC_ENGINEER_SERVER_BATCH_TASK_H
+#define ACC_ENGINEER_SERVER_BATCH_TASK_H
+#include "detail/batch_task.h"
+
+namespace acc_engineer::rpc {
+
+using detail::batch_task;
+
+}
+#endif // ACC_ENGINEER_SERVER_BATCH_TASK_H
